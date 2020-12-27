98e823cbdc186874a34deff4262636474ceffc59
yoshiki <yoshiki0911@gmail.com>
2020-12-27 11:30:34 +0900

    #1 chore: setup repository

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = '{project_name}'
-copyright = '2020, {author}'
-author = '{author}'
+project = 'tech_shared_code'
+copyright = '2020, gsy0911'
+author = 'gsy0911'
 
 
 # -- General configuration ---------------------------------------------------
